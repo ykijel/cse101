dad12f22360b5af92889a8d7bf3024d34bb01324
Yahli Kijel <ykijel@ucsc.edu>
2023-10-14 00:20:24 -0700

    Add Graph.h

---
+++ pa2/Graph.h
@@ -0,0 +1,34 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <stdbool.h>
+#include "List.h"
+
+typedef struct GraphObj* Graph;
+
+Graph newGraph(int n);
+
+void freeGraph(Graph* pG);
+
+int getOrder(Graph G);
+
+int getSize(Graph G);
+
+int getSource(GraphG);
+
+int getParent(Graph G, int u);
+
+int getDist(Graph G, int u);
+
+void getPath(List L, Graph G, int u);
+
+void makeNull(Graph G);
+
+void addEdge(Graph G, int u, int v);
+
+void addArc(Graph G, int u, int v);
+
+void BFS(Graph G, int s);
+
+void printGraph(FILE* out, Graph G);
+
+#endif
