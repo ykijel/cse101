38c6d5d73f05ddffda626d22f165e2ee312355ec
Yahli Kijel <ykijel@ucsc.edu>
2023-11-01 23:30:52 -0700

    Add new file

---
+++ pa4/Matrix.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+typedef struct MatrixObj* Matrix;
+
+Matrix newMatrix(int n);
+
+void freeMatrix(Matrix* pM);
+
+int size(Matrix M);
+
+int NNZ(Matrix M);
+
+int equals(Matrix A, Matrix B);
+
+void makeZero(Matrix M);
+
+void changeEntry(Matrix M, int i, int j, double x);
+
+Matrix copy(Matrix A);
+
+Matrix transpose(Matrix A);
+
+Matrix scalarMult(double x, Matrix A);
+
+Matrix sum(Matrix A, Matrix B);
+
+Matrix diff(Matrix A, Matrix B);
+
+Matrix product(Matrix A, Matrix B);
+
+void printMatrix(FILE* out, Matrix M);
