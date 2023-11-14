08f686d0c56708e9d379d5ce7ba4ebe55171f5ee
Yahli Kijel <ykijel@ucsc.edu>
2023-11-14 02:36:03 -0800

    Update 2 files
    - /pa5/List.cpp
    - /pa5/Shuffle.cpp

--- pa5/Shuffle.cpp
+++ pa5/Shuffle.cpp
@@ -1,70 +1,100 @@
-#include "List.h"  // Include the definition of your List class
+#include "List.h"
+#include <iostream>
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <max_deck_size>" << std::endl;
-        return 1;
+// Function to perform a single shuffle on the given list
+void shuffle(List &D)
+{
+    int mid = D.length() / 2;
+    int i;
+    List E;
+    D.moveBack();
+
+    // Insert the latter half of the list first
+    for (i = D.length() - 1; i >= mid; i--)
+    {
+        E.insertAfter(D.movePrev());
     }
 
-    int maxDeckSize = std::stoi(argv[1]);
+    // Prepare for merge
+    E.moveFront();
+    D.moveFront();
+    if (mid <= 1)
+    {
+        E.moveNext();
+        if (D.position() < D.length())
+        {
+            E.insertBefore(D.moveNext());
+        }
+    }
+    else
+    {
+        for (i = 0; i < mid; i++)
+        {
+            E.moveNext();
+            if (D.position() < D.length())
+            {
+                E.insertBefore(D.moveNext());
+            }
+        }
+    }
+
+    // Check if cursor is not at the back before assigning D to E
+    if (D.position() != D.length())
+    {
+        // Move cursor to the back before assigning D to E
+        D.moveFront();
+        D = E;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Check command line for correct number of arguments
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <max_deck_size>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int maxDeckSize = 0;
+
+    try
+    {
+        maxDeckSize = std::stoi(argv[1]);
+    }
+    catch (std::invalid_argument &e)
+    {
+        std::cerr << "Usage: " << argv[0] << " <max_deck_size>" << std::endl;
+        return EXIT_FAILURE;
+    }
 
     std::cout << "deck size\tshuffle count" << std::endl;
-    std::cout << "----------------------------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
 
-    // Perform shuffles for each value of n from 1 to maxDeckSize
-    for (int n = 1; n <= maxDeckSize; ++n) {
-        List deck;
+    // Perform shuffles for each deck size
+    for (int n = 1; n <= maxDeckSize; n++)
+    {
+        List L;
 
-        // Initialize the deck with values 0 to n-1
-        for (int i = 0; i < n; ++i) {
-            deck.insertAfter(i);
+        // Initialize the list with values 0 to n-1
+        for (int i = 0; i < n; i++)
+        {
+            L.insertBefore(i);
         }
 
-        List originalOrder = deck;  // Store the original order
-
-        // Perform shuffles until the original order is reached
         int shuffleCount = 0;
-        do {
-            // Implement the shuffle operation as described in the prompt
-            List shuffledDeck;
+        List C = L;
 
-            // Split the deck into two halves
-            int middle = n / 2;
-
-            List leftHalf;
-            List rightHalf;
-
-            for (int i = 0; i < middle; ++i) {
-                leftHalf.insertAfter(deck.front());
-                deck.eraseAfter();
-            }
-
-            while (deck.length() != 0) {
-                rightHalf.insertAfter(deck.front());
-                deck.eraseAfter();
-            }
-
-            // Merge the two halves
-            while (leftHalf.length() != 0 || rightHalf.length() != 0) {
-                if (leftHalf.length() != 0) {
-                    shuffledDeck.insertBefore(leftHalf.front());
-                    leftHalf.eraseAfter();
-                }
-
-                if (rightHalf.length() != 0) {
-                    shuffledDeck.insertBefore(rightHalf.front());
-                    rightHalf.eraseAfter();
-                }
-            }
-
-            deck = shuffledDeck;  // Update the deck with the shuffled order
+        // Loop until the list is back to its original order
+        do
+        {
+            shuffle(L);
             shuffleCount++;
+        } while (!(L == C));
 
-        } while (!deck.equals(originalOrder));  // Check if the deck is back to its original order
-
-        // Print the result for the current n
+        // Print the results for the current deck size
         std::cout << " " << n << "\t\t" << shuffleCount << std::endl;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
